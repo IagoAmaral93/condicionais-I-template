3f2ddb65b7b133626d7e8bfba75c13177d6ad985
iagoamaral93 <iagoamaral1993@gmail.com>
2023-02-27 21:13:46 -0300

    condicionais-template

---
+++ script.js
@@ -0,0 +1,95 @@
+//                  EXERCICIO 1
+// const velocidade = 50
+// console.log(`a sua velocidade é ${celocidade} km/h`)
+
+// if (velocidade > 80)  {
+//     console.log(`voce esta acima da velocidade permitida. `)
+// }
+
+// // EXERCICIO | CONDICAO COMPSOTA
+// const estaChovendo = true
+
+// if(estaChovendo === true) {
+//     console.log(`a janalea ficou aberta =( )`)
+//   }  else {
+//         console.log(`hmmm solzin bora f1`)
+//     }
+
+
+
+//          PRATICA GUIADA 1 
+
+// const booleano1 = false
+// const booleano2 = true
+// if (booleano1) {
+// alert(`Booleano 1 é true lek`)
+// } else {
+//     alert(`booleano 1 é FALSE`)
+// }
+
+// const booleano3 = true
+
+// if(booleano1 === booleano2){
+//     alert(`Booleano 1 e Booleano 2 sao IGUAIS`)
+// } else if(booleano2 === booleano3) {
+//         alert(`Booleano 2 e Booleano 3 sao IGUAIS`)
+// } else if(booleano1 === booleano3) {
+//     alert(`Vooleano 1 e booleano 3 sao Iguais`)
+// } else {
+//     alert(`Nao ecistem valore iguais`)
+// }
+
+// exmplo BOLINHO DE CHUVA
+
+// const temOvo = true
+// const temFarinha = true 
+// const temLeite = true 
+// const temAcucar = true
+
+// if(!temOvo){
+//     console.log(`ovo:`, temOvo)
+//     console.log(`nao tem ovo, rola bolo`)
+// } else if(!temFarinha){
+// console.log(`farinha`, temFarinha)
+// console.log(`tem ovo, mas nao tem farinha.Deixa quieto`)
+// } else if(!temLeite){
+//     console.log(`leite`, temLeite)
+//     console.log(`Mesm com a farinha e os ovos, nao tem leote. Nao da pra fazer obolin`)
+
+// }else if(!temAcucar){
+//     console.log(`Acucar`, temAcucar)
+//     console.log(`Olha agt tem tudo menos o acucuar, entao nao rola`)
+// }else {
+//     console.log(`Agora tu consegue meu consagrado!`)
+// }
+
+
+
+
+// PRATICA GUIADA 2
+
+// const idade = 29
+ 
+// if (idade < 16) {
+//     console.log(`Voce NAO pode votar`)
+// }else if(idade < 18 || idade >= 70){
+//     console.log(`Voce pode votar - FACULTATIVO`)
+// }else if (idade >= 18) {
+//     console.log(`voce DEVE votar`)
+//  } else {
+//     console.log(`dado invalido`)
+//  }
+
+
+// exercicio de ficacao 
+const media = 7
+
+if (media >= 5) {
+  console.log("VOCE FOI Aprovado")
+} else if (media >= 3) {
+  console.log("VOCE ESTA EM Recuperação")
+} else if (media < 3) {
+  console.log("VOCE FOI REPROVADO")
+} else {
+  console.log("Dado inválido")
+}
